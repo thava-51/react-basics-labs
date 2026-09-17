f9ef2ce8933f19db456fc26223faaefa534b0ac7
thava-51 <20120856@setu.ie>
2026-09-17 12:08:55 +0100

    Tasky Lab 1

--- rbasics/src/App.jsx
+++ rbasics/src/App.jsx
@@ -1,24 +1,13 @@
-import React, { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import './App.css';
+import Task from './components/Task';
 
 function App() {
-  const [taskState, setTaskState] = useState({
-    tasks: [
-      {title:"Dishes", description:"Empty dishwasher", deadline:"Today"},
-      {title:"Laundry", description:"Fold clothes and put away", deadline:"Tomorrow"},
-      {title:"Tidy up", deadline:"Today"}
-    ]
-  });
-
   return (
     <div className='container'>
       <h1>Tasky</h1>
-      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description}></Task>
-      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description}></Task>
-      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description}></Task>
+      <Task title="Dishes" deadline="Today" description="Use the dishwasher" />
+      <Task title="Laundry" deadline="Tomorrow" description="Washing machine in demand" />
+      <Task title="Tidy" deadline="Today" description="You can do it" />
     </div>
   );
 }
---
+++ rbasics/src/components/Task.jsx
@@ -0,0 +1,11 @@
+function Task(props){
+    return (
+       <div className="card">
+        <p className="title">{props.title}</p>
+        <p>Due: {props.deadline}</p>
+        <p>{props.description}</p>
+       </div>
+    )
+}
+
+export default Task;
