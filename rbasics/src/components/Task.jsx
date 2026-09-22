155374add0ffafea1ce8ba3107debe5a5af6542b
thava-51 <20120856@setu.ie>
2026-09-22 11:22:46 +0100

    Tasky Lab 2B Part 1

--- rbasics/src/components/Task.jsx
+++ rbasics/src/components/Task.jsx
@@ -1,10 +1,11 @@
 function Task(props){
     return (
-       <div className="card">
+       <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
         <p className="title">{props.title}</p>
         <p>Due: {props.deadline}</p>
         <p>{props.description}</p>
         <p>Priority: {props.priority}</p>
+        <button onClick={props.markDone} className='doneButton'>Done</button>
        </div>
     )
 }
