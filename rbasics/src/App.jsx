191b9fdf4f277ab1f5f5f2950fe715e99ab37b27
thava-51 <20120856@setu.ie>
2026-09-22 11:46:02 +0100

    Tasky Lab 2B Part 3

--- rbasics/src/App.jsx
+++ rbasics/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
+import AddTaskForm from './components/Form';
+
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -38,6 +40,7 @@ function App() {
           deleteTask = {() => deleteHandler(index)}
         />
       ))}
+      <AddTaskForm/>
     </div>
   );
 }
